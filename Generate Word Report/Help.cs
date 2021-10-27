05a1a4610c985a3ab897cefad719621531960924
linlijian <praw__rul2__lvluk@outlook.co.th>
2021-10-27 11:29:28 +0700

    BitmapToBase64String

--- Generate Word Report/Help.cs
+++ Generate Word Report/Help.cs
@@ -664,5 +664,20 @@ namespace Generate_Word_Report
 
             return bmpReturn;
         }
+        public static string BitmapToBase64String(this string path)
+        {
+            using (Image image = Image.FromFile(path))
+            {
+                using (MemoryStream m = new MemoryStream())
+                {
+                    image.Save(m, image.RawFormat);
+                    byte[] imageBytes = m.ToArray();
+
+                    // Convert byte[] to Base64 String
+                    string base64String = Convert.ToBase64String(imageBytes);
+                    return base64String;
+                }
+            }
+        }
     }
 }
